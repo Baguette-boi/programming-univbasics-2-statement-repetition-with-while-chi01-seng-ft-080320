15f543a837abac050612c8cfcb065b7439eab4f2
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-06-30 20:56:16 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -3,7 +3,6 @@ count = 10
 while count > 0 do
   break if count == countdown_numb
     puts "#{count}\n"
-  if count == 0
-    puts "Happy New Year!"
   count = count- 1 
 end
+puts "Happy New Year!"
